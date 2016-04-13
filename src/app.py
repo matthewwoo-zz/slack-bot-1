9030ed91d8248c7580168b12582b06f0c62ad0ba
matthewwoo <matthew.edan.woo@gmail.com>
2016-04-13 11:26:30 -0700

    Reading slack messages

--- src/app.py
+++ src/app.py
@@ -1,15 +1,31 @@
-import json
-from slackclient import SlackClient
+# import json
+# from slackclient import SlackClient
+#
+# token = "xoxb-34239333142-HspVW30XI57E1o6aQqHZpwuX"
+# sc = SlackClient(token)
+# message = sc.api_call("channels.history", channel="C103W0BRC", count=1)
+# raw_message = json.dumps(message)
+# print json_message
+#
+# print json_message['messages'][0]['text']
+# json_message = json.loads(raw_message)
 
-token = "xoxb-34239333142-HspVW30XI57E1o6aQqHZpwuX"
-sc = SlackClient(token)
-message = sc.api_call("channels.history", channel="C103W0BRC", count=1)
-raw_message = json.dumps(message)
-json_message = json.loads(raw_message)
 
-print json_message
 
-print json_message['messages'][0]['text']
+#
+# import time, json, yaml, re, os
+# from slackclient import SlackClient
+# token = "xoxb-34239333142-HspVW30XI57E1o6aQqHZpwuX"
+# sc = SlackClient(token)
+#
+# while True:
+#   new_evts = sc.rtm_read()
+#   for evt in new_evts:
+#       print(evt)
+#       if "type" in evt:
+#         if evt["type"] == "message" and "text" in evt:
+#           message=evt["text"]
+#       time.sleep(3)
 
 # print sc.api_call("channels.info", channel="C103W0BRC")
 # print sc.api_call(
@@ -18,15 +34,16 @@ print json_message['messages'][0]['text']
 # )
 
 
-# import time
-# import requests
-# from slackclient import SlackClient
-# token = "xoxp-34138599623-34132008758-34138148288-4cb67550a9"
-# sc = SlackClient(token)
-# if sc.rtm_connect():
-#     while True:
-#         print sc.rtm_read()
-#         time.sleep(1)
-# else:
-#     print "Connection Failed, invalid token?"
+import time
+import requests
+from slackclient import SlackClient
+token = "xoxp-34138599623-34132008758-34138148288-4cb67550a9"
+sc = SlackClient(token)
+if sc.rtm_connect():
+    while True:
+        print sc.rtm_read()
+        if sc.rtm_read():
+            time.sleep(3)
+else:
+    print "Connection Failed, invalid token?"
 
--- src/models/articles/article.py
+++ src/models/articles/article.py
@@ -1,7 +1,15 @@
+import datetime
+
 
 class Article(object):
-    def __int__(self):
-        """
-        URL
-        """
+    def __int__(self, url, slack_channel, earliest_time=None, date=None):
+        self.url = url
+        self.date = datetime.datetime() if date is None else date
+        self.slack_channel = slack_channel
+        self.time_period = datetime.datetime() - datetime.timedelta(days=1) if earliest_time is None else earliest_time
+
+
+    # """
+    # Example: slack_channel
+    # """
 
