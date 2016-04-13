dd1af999da5a08bdf1131bde0ee0b27d58c753a9
matthewwoo <matthew.edan.woo@gmail.com>
2016-04-12 22:36:41 -0700

    Parse for url in channel history

--- src/app.py
+++ src/app.py
@@ -1,24 +1,32 @@
-# import time
-# from slackclient import SlackClient
-#
-# token = "xoxp-34138599623-34132008758-34138148288-4cb67550a9"
-# sc = SlackClient(token)
-# print sc.api_call("api.test")
-# print sc.api_call("channels.info", channel="1234567890")
+import json
+from slackclient import SlackClient
+
+token = "xoxb-34239333142-HspVW30XI57E1o6aQqHZpwuX"
+sc = SlackClient(token)
+message = sc.api_call("channels.history", channel="C103W0BRC", count=1)
+raw_message = json.dumps(message)
+json_message = json.loads(raw_message)
+
+print json_message
+
+print json_message['messages'][0]['text']
+
+# print sc.api_call("channels.info", channel="C103W0BRC")
 # print sc.api_call(
 #     "chat.postMessage", channel="#general", text="Hello from Python! :tada:",
 #     username='pybot', icon_emoji=':robot_face:'
 # )
-#
 
-import time
-from slackclient import SlackClient
 
-token = "xoxp-34138599623-34132008758-34138148288-4cb67550a9"
-sc = SlackClient(token)
-if sc.rtm_connect():
-    while True:
-        print sc.rtm_read()
-        time.sleep(1)
-else:
-    print "Connection Failed, invalid token?"
+# import time
+# import requests
+# from slackclient import SlackClient
+# token = "xoxp-34138599623-34132008758-34138148288-4cb67550a9"
+# sc = SlackClient(token)
+# if sc.rtm_connect():
+#     while True:
+#         print sc.rtm_read()
+#         time.sleep(1)
+# else:
+#     print "Connection Failed, invalid token?"
+
---
+++ src/models/users/user.py
@@ -0,0 +1,7 @@
+
+class User(object):
+    def __int__(self):
+        """
+        Instapaper API Key
+        """
+
